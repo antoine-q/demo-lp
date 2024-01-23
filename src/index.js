d15245834c9ba97890728d744a93528f2a938c4d
Mathis_Rimbert <mathis.rimbert@centrale.centralelille.fr>
2024-01-23 11:59:39 +0100

    add caroussel realisations

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ import './style.css';
 import { Splide } from '@splidejs/splide';
 import { AutoScroll } from '@splidejs/splide-extension-auto-scroll';
 
-new Splide( '#caroussel-partenaire', {type    : 'loop', autoplay: 'pause', perPage : 6, arrows: false, pagination: false}).mount( { AutoScroll } );
+new Splide( '#caroussel-partenaire', {type    : 'loop', autoplay: 'pause', perPage : 6, arrows: false, pagination: false, pauseOnHover: false, pauseOnFocus: false}).mount( { AutoScroll } );
 
 document.addEventListener( 'DOMContentLoaded', function () {
     new Splide( '#image-carousel', {arrows: false} ).mount();
   } );
+
+  new Splide('#caroussel-real', {type: 'loop', autoplay: 'true', interval:'4000', arrows: false, pagination: false, perPage : 4,  pauseOnHover: false,}).mount();
