2733ed10ecd537f32a2e544eeea2f3467b729dbf
Mathis_Rimbert <mathis.rimbert@centrale.centralelille.fr>
2024-01-23 11:32:44 +0100

    add caroussel partenaire

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './style.css';
+
+
+import { Splide } from '@splidejs/splide';
+import { AutoScroll } from '@splidejs/splide-extension-auto-scroll';
+
+new Splide( '#caroussel-partenaire', {type    : 'loop', autoplay: 'pause', perPage : 6, arrows: false, pagination: false}).mount( { AutoScroll } );
+
+document.addEventListener( 'DOMContentLoaded', function () {
+    new Splide( '#image-carousel', {arrows: false} ).mount();
+  } );
