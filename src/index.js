0b839440af90ca1c862a7f9f01eea19ed9952e17
Mathis_Rimbert <mathis.rimbert@centrale.centralelille.fr>
2024-01-24 11:58:24 +0100

    Add animation partie etapes

--- src/index.js
+++ src/index.js
@@ -9,67 +9,50 @@ let rapide = document.getElementById('rapide');
 let rentable = document.getElementById('rentable');
 let d1 = document.getElementById('d1')
 
-new Splide( '#caroussel-partenaire', {type    : 'loop', autoplay: 'pause', perPage : 6, arrows: false, pagination: false, pauseOnHover: false, pauseOnFocus: false}).mount( { AutoScroll } );
 
 document.addEventListener( 'DOMContentLoaded', function () {
-    new Splide( '#image-carousel', {arrows: false} ).mount();
-  } );
+    new Splide( '#image-carousel', {arrows: false, autoplay: true, interval:4000} ).mount();
+    new Splide('#caroussel-real', {type: 'loop', autoplay: 'true', interval:'4000', arrows: false, pagination: false, pauseOnHover: false, autoWidth: true, gap: "4em"}).mount();
+    new Splide( '#caroussel-partenaire', {type    : 'loop', autoplay: 'pause', perPage : 6, arrows: false, pagination: false, pauseOnHover: false, pauseOnFocus: false}).mount( { AutoScroll } );
 
-  new Splide('#caroussel-real', {type: 'loop', autoplay: 'true', interval:'4000', arrows: false, pagination: false, pauseOnHover: false, autoWidth: true, gap: "4em"}).mount();
 
-  d3.style.display = "none";
-  d2.style.display = "none";
-  d1.style.display="block";
-
-  facile.addEventListener("click",() =>{
-    if(getComputedStyle(d1).display != "none"){
-      facile.style.backgroundColor = "#E02B88";
-      facile.style.color ="white";
-      facile.style.textDecorationColor = "#E02B88"
-    } else {
-      d3.style.display = "none";
-      d2.style.display = "none";
-      d1.style.display="block";
-      facile.style.backgroundColor = "#E02B88";
-      facile.style.color ="white";
-      facile.style.textDecorationColor = "#E02B88"
-      rapide.style.backgroundColor = "white";
-      rapide.style.color ="#E02B88";
-      rentable.style.backgroundColor = "white";
-      rentable.style.color ="#E02B88";
-    }
-  })
-
-  rapide.addEventListener("click",()=>{
-    if(getComputedStyle(d2).display != "none"){
-    } else {
-      d3.style.display = "none";
-      d2.style.display = "block";
-      d1.style.display="none";
-      rapide.style.backgroundColor = "#E02B88";
-      rapide.style.color ="white";
-      rapide.style.textDecorationColor = "#E02B88"
-      facile.style.backgroundColor = "white";
-      facile.style.color ="#E02B88";
-      rentable.style.backgroundColor = "white";
-      rentable.style.color ="#E02B88";
-    }
-  })
-
-  rentable.addEventListener("click",()=>{
-    if(getComputedStyle(d3).display != "none"){
+    let scrollPos = [4050,4550,4750,5050,5250];
+    const etapes = document.querySelector('.d-etapes');
+    const etapesListe = document.querySelectorAll('.d-etapes');
+    
+    function checkPosition(){
+      let windowY = window.scrollY;
       
-    } else {
-      d3.style.display = "block";
-      d2.style.display = "none";
-      d1.style.display="none";
-      rentable.style.backgroundColor = "#E02B88";
-      rentable.style.color ="white";
-      rentable.style.textDecorationColor = "#E02B88"
-      rapide.style.backgroundColor = "white";
-      rapide.style.color ="#E02B88";
-      facile.style.backgroundColor = "white";
-      facile.style.color ="#E02B88";
-    }
-  })
+      etapesListe.forEach((etapesElement, keyElement) => {
+        if(windowY < scrollPos[keyElement]) {
+          etapesElement.classList.add('zt-step');
+        } else {
+          etapesElement.classList.remove('zt-step');
+        }
+      })
+      }
+    
+    window.addEventListener("scroll", checkPosition);
+      
+
+    const onglets = document.querySelectorAll('.d-title');
+    const panneaux = document.querySelectorAll('div[class^="panneau-"]');
+  
+    onglets.forEach((ongletElement, ongletKey) => {
+      ongletElement.addEventListener('click', function () {
+        onglets.forEach(element => { element.classList.remove('zt-selected') });
+        ongletElement.classList.add('zt-selected');
+        panneaux.forEach((panneauElement, panneauKey) => {
+          panneauElement.classList.add('hidden');
+          if (panneauKey === ongletKey) {
+            panneauElement.classList.remove('hidden');
+          }
+        });
+      });
+    });
+
+} );
+
+
+ 
 
